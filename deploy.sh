97f73a9969974dc0cd2ad8446641e25e2ae2570e
Eugenio Lattanzio <eugenio63@gmail.com>
2019-03-20 12:31:42 -0300

    styles

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+TAR_FILE=`npm pack -s`
+echo $TAR_FILE
+scp $TAR_FILE workaround@william-paca.dreamhost.com:~/
+ssh workaround@william-paca.dreamhost.com "rm -rf package && tar -zxf $TAR_FILE && mv package/dist/* workaround.club/public/"
