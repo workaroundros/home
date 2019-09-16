8a42906456db645c60752311b78584a352f51c90
Eugenio Lattanzio <eugenio63@gmail.com>
2019-09-16 14:18:54 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 npm run build
 TAR_FILE=`npm pack -s`
 echo $TAR_FILE
-scp $TAR_FILE workaround@william-paca.dreamhost.com:~/
-ssh workaround@william-paca.dreamhost.com "rm -rf package && tar -zxf $TAR_FILE && mv package/dist/* workaround.club/public/"
+scp $TAR_FILE euglat1@william-paca.dreamhost.com:~/
+ssh euglat1@william-paca.dreamhost.com "rm -rf package && tar -zxf $TAR_FILE && mv package/dist/* workaround.com.ar/"
